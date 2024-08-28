a912545e502fc67bd888c2eae82691e37c55c207
zeraneL <farizrifqi26@gmail.com>
2024-08-28 18:57:09 +0800

    remove single

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import BlumBot from "./src/Blum/BlumBot";
-import { log } from "./src/log";
-import dotenv from "dotenv";
-dotenv.config();
-const init = async () => {
-  if (!process.env.TG_QUERY) return;
+import fs from "node:fs";
+const loadQuery = () => {
+  return fs
+    .readFileSync("query.txt", "utf8")
+    .split("\n")
+    .filter((q) => q !== "");
+};
+const runBot = async (q: string) => {
+  const blum = new BlumBot(q);
+  await blum.run();
+};
+const start = async () => {
   console.clear();
-  const blum = new BlumBot(process.env.TG_QUERY);
-  console.log(await blum.getUserInfo());
+  const loadedQuery = loadQuery();
+  console.log(loadedQuery.length + " queries loaded");
+  await Promise.all(loadedQuery.map((q) => runBot(q)));
 };
 
-init();
+start();
