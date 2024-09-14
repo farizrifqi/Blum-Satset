feb366a12cff30fb5cba3842c720e6f3a27e9dff
zeraneL <farizrifqi26@gmail.com>
2024-09-15 01:25:52 +0800

    verify new quest

--- src/Blum/BlumBot.ts
+++ src/Blum/BlumBot.ts
@@ -829,6 +829,7 @@ export default class BlumBot {
           const keywords = {
             "38f6dd88-57bd-4b42-8712-286a06dac0a0": "VALUE",
             "6af85c01-f68d-4311-b78a-9cf33ba": "GO GET",
+            "d95d3299-e035-4bf6-a7ca-0f71578e9197": "BEST PROJECT EVER",
           };
           await Promise.all(
             tasks
